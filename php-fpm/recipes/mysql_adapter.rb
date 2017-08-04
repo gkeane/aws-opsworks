0f90b72b9e6a12cf3ab98ded14e9b6a1c8231371
Greg Keane <gkeane@gmail.com>
2017-08-04 12:11:57 -0400

    mysql

--- php-fpm/recipes/mysql_adapter.rb
+++ php-fpm/recipes/mysql_adapter.rb
@@ -1,6 +1,6 @@
 package 'php-mysql' do
   package_name value_for_platform_family(
-    'rhel' => 'php7-mysql',
-    'debian' => 'php7-mysql'
+    'rhel' => 'php70-mysqlnd',
+    'debian' => 'php70-mysqlnd'
   )
 end
